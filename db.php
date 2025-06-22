ebbc1096ad8e304fcc192be9893adb76dd91d4d2
Loai AlQadasi <167661779+Loaialqadasi@users.noreply.github.com>
2025-06-22 21:25:35 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-<?php 
-// InfinityFree MySQL credentials
-$host = 'dpg-d1bqnqodl3ps73eupri0-a'; 
-$username = 'lostandfound'; 
-$password = 'bXH9PCnHXpCcnXqTXfpKt41cESxQzYFs'; // Replace with your real password
-$database = 'shabab'; 
-=======
 <?php
+// PostgreSQL credentials for Render.com
 $host = 'dpg-d1bqnqodl3ps73eupri0-a.oregon-postgres.render.com';
 $port = '5432';
 $dbname = 'shabab';
 $user = 'lostandfound';
 $password = 'bXH9PCnHXpCcnXqTXfpKt41cESxQzYFs';
->>>>>>> 8de350a (Anything)
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
