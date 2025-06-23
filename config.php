4d7900bd136401a5d048a6825936f55456eaa973
Loai AlQadasi <167661779+Loaialqadasi@users.noreply.github.com>
2025-06-23 19:09:06 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 // config.php
 // Store your DeepInfra AI API configuration here
 
-define('AI_API_KEY', 'srSgL1D9BK80YrE0xi4vc6nuMmBf1hNY');
+define('AI_API_KEY', '74GkCTfYTQ2kzU6PHoossvhTvPVf7pGE');
 define('AI_API_ENDPOINT', 'https://api.deepinfra.com/v1/openai/chat/completions');
